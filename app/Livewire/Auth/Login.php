aa499b1f6cfd8c7a27253d0d374784ac12938c04
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2023-12-14 16:02:39 -0300

    crm-2: ensuring login to be secured by rate limiting

--- app/Livewire/Auth/Login.php
+++ app/Livewire/Auth/Login.php
@@ -21,14 +21,33 @@ class Login extends Component
 
     public function tryToLogin(): void
     {
+        if ($this->ensureIsNotRateLimiting()) {
+            return;
+        }
         if (! Auth::attempt(['email' => $this->email, 'password' => $this->password])) {
-            RateLimiter::hit(Str::lower($this->email));
-
+            RateLimiter::hit($this->throttleKey());
             $this->addError('invalidCredentials', trans('auth.failed'));
 
             return;
         }
-
         $this->redirect(route('dashboard'));
+    }
+
+    private function throttleKey(): string
+    {
+        return Str::transliterate(Str::lower($this->email).'|'.request()->ip());
+    }
+
+    private function ensureIsNotRateLimiting(): bool
+    {
+        if (RateLimiter::tooManyAttempts($this->throttleKey(), 5)) {
+            $this->addError('rateLimiter', trans('auth.throttle', [
+                'seconds' => RateLimiter::availableIn($this->throttleKey()),
+            ]));
+
+            return true;
+        }
+
+        return false;
     }
 }
--- app/Livewire/Welcome.php
+++ app/Livewire/Welcome.php
@@ -3,17 +3,12 @@
 namespace App\Livewire;
 
 use Illuminate\Contracts\View\View;
-use Illuminate\Support\Facades\Artisan;
 use Livewire\Component;
 
 class Welcome extends Component
 {
     public function render(): View
     {
-        ds()->commandsOn('running a command');
-        Artisan::call('inspire');
-        ds()->commandsOff();
-
         // ds()->model($user);
         return view('livewire.welcome');
     }
