95ea5283857cefbe45908ce3b261596794e5cf98
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2023-12-14 10:42:16 -0300

    crm-2: be able to successfully log in

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,6 @@ use App\Livewire\Welcome;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', Welcome::class);
+Route::get('/', Welcome::class)->name('dashboard');
 Route::get('/register', Register::class)->name('auth.register');
 Route::get('/logout', fn () => Auth::logout())->name('logout');
