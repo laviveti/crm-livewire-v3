c1e515a30c0d962314f93379bd96a5874e16b59b
Thiago Rocha <thiago_srocha01@hotmail.com>
2024-01-05 14:41:54 -0300

    crm-4: checking form rules

--- app/Livewire/Auth/Password/Reset.php
+++ app/Livewire/Auth/Password/Reset.php
@@ -2,23 +2,38 @@
 
 namespace App\Livewire\Auth\Password;
 
+use App\Models\User;
+use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Contracts\View\View;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Str;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
 
 class Reset extends Component
 {
+    #[Rule(['required'])]
     public ?string $token = null;
 
-    public function mount(): void
+    #[Rule(['required', 'email', 'confirmed'])]
+    public ?string $email = null;
+
+    public ?string $email_confirmation = null;
+
+    #[Rule(['required', 'confirmed'])]
+    public ?string $password = null;
+
+    public ?string $password_confirmation = null;
+
+    public function mount(?string $token = null, ?string $email = null): void
     {
-        $this->token = request('token');
+        $this->token = request('token', $token);
+        $this->email = request('email', $email);
 
         if ($this->tokenNotValid()) {
-
             session()->flash('status', 'Token Invalid');
-
             $this->redirectRoute('login');
         }
     }
@@ -28,11 +43,28 @@ class Reset extends Component
         return view('livewire.auth.password.reset');
     }
 
+    public function updatePassword(): void
+    {
+        $this->validate();
+        $status = Password::reset(
+            $this->only('email', 'password', 'password_confirmation', 'token'),
+            function (User $user, $password) {
+                $user->password = $password;
+                $user->remember_token = Str::random(60);
+                $user->save();
+
+                event(new PasswordReset($user));
+            }
+        );
+        $this->redirect(route('dashboard'));
+
+        session()->flash($status, __($status));
+    }
+
     private function tokenNotValid(): bool
     {
         $tokens = DB::table('password_reset_tokens')
             ->get(['token']);
-
         foreach ($tokens as $t) {
             if (Hash::check($this->token, $t->token)) {
                 return false;
