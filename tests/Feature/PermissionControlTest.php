33b4cb717eeceb47fc1812248de8c33a091a0fa4
Thiago Rocha <thiago_srocha01@hotmail.com>
2024-01-10 11:45:42 -0300

    crm-5: creating a seed for permissions

--- tests/Feature/PermissionControlTest.php
+++ tests/Feature/PermissionControlTest.php
@@ -2,21 +2,20 @@
 
 use App\Models\Permission;
 use App\Models\User;
+use Database\Seeders\PermissionSeeder;
+use Tests\TestCase;
 
 use function Pest\Laravel\assertDatabaseHas;
 
 it('should be able to give an user a permission to do something', function () {
     $user = User::factory()->create();
     $user->givePermissionTo('be an admin');
-
     expect($user)
         ->hasPermissionTo('be an admin')
         ->toBeTrue();
-
     assertDatabaseHas('permissions', [
         'key' => 'be an admin',
     ]);
-
     assertDatabaseHas(
         'permission_user',
         [
@@ -25,4 +24,12 @@ it('should be able to give an user a permission to do something', function () {
             // 'permission_id' => Permission::where(['key' => 'be an admin'])->first()->id (alternativa em array)
         ]
     );
+});
+
+test('permission has to have a seeder', function () {
+    /** @var TestCase $this */
+    $this->seed(PermissionSeeder::class);
+    assertDatabaseHas('permissions', [
+        'key' => 'be an admin',
+    ]);
 });
